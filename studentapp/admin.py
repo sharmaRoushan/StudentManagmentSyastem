4efda1aac1303e8b43184b7a37256a575f401e37
Roushankumar <roushankumar732389@gmail.com>
2023-08-30 07:49:14 +0530

    This is my new project

---
+++ studentapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import*
+from django.contrib.auth.admin import UserAdmin
+
+admin.site.register(CoustamUser,UserAdmin)
+class UserModel(UserAdmin):
+    list_display=['username','user_type']
+
+# Register your models here.
---
+++ studentmagmentsystem/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from .import views,Hod_views,Staff_views,Student_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('base',views.base,name='base'),
+    path('singhup',views.singhup,name='singhup'),
+    #  login path
+    path('',views.login,name='login'),
+    path('dologin',views.dologin,name='dologin'),
+
+
+
+    
+
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
