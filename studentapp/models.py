55b550794cbdd9f1271dd49dfaf103eda8cec23a
Roushankumar <roushankumar732389@gmail.com>
2023-09-22 17:53:14 +0530

    fix the data bse [problem

--- studentapp/models.py
+++ studentapp/models.py
@@ -67,6 +67,18 @@ class Staff_notification(models.Model):
    
     def __str__(self):
         return self.staff_id.admin.first_name
+class staff_leave(models.Model):
+    staff_id=models.ForeignKey(Staff,on_delete=models.CASCADE)
+    data=models.CharField(max_length=100)
+    message=models.TextField()
+    status=models.IntegerField(default=0)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    # def __str__(self):
+    #     return self.staff_id.admin.first_name + self.staff_id.admin.last_name
+
+
+
 
 
 
